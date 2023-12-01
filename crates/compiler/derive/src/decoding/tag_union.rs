d4fcbae0f2217c01e58c49e61b4252583aa0a7e1
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2023-11-30 20:21:12 -0600

    wip finalizer

--- crates/compiler/derive/src/decoding/tag_union.rs
+++ crates/compiler/derive/src/decoding/tag_union.rs
@@ -1,11 +1,14 @@
-use roc_can::expr::Expr;
+use roc_can::expr::WhenBranchPattern;
+use roc_can::expr::{Expr, WhenBranch};
+use roc_can::pattern::{DestructType, Pattern, RecordDestruct};
 
-use crate::util::Env;
+use crate::util::{Env, ExtensionKind};
 use roc_module::called_via::CalledVia;
 use roc_module::ident::TagName;
 use roc_module::symbol::Symbol;
 use roc_region::all::Loc;
-use roc_types::subs::Variable;
+use roc_region::all::Region;
+use roc_types::subs::{ExhaustiveMark, RedundantMark, Variable};
 
 // We want to generate an implementation like this;
 
@@ -87,7 +90,11 @@ pub(crate) fn decoder(
     _def_symbol: Symbol,
     tags: Vec<(TagName, u16)>,
 ) -> (Expr, Variable) {
-    name_decoder(env, tags)
+    // name_decoder(env, tags)
+    let var1 = env.subs.fresh_unnamed_flex_var();
+    let var2 = env.subs.fresh_unnamed_flex_var();
+
+    finalizer(env, "Foo".into(), vec![var1, var2])
 }
 
 //     nameDecoder =
@@ -165,3 +172,82 @@ fn name_decoder(env: &mut Env, tags: Vec<(TagName, u16)>) -> (Expr, Variable) {
 //     },
 //     env.subs.fresh_unnamed_flex_var(),
 // )
+
+// finalizer = \s ->
+//     when s is
+//         { e0: Ok v0, e1: Ok v1 } -> Ok (Two v0 v1)
+//         _ -> Err TooShort
+fn finalizer(env: &mut Env, tag_name: TagName, payload_vars: Vec<Variable>) -> (Expr, Variable) {
+    let when_var = env.subs.fresh_unnamed_flex_var();
+    let s_arg_symbol = env.new_symbol("s");
+    let s_arg_variable = env.subs.fresh_unnamed_flex_var();
+
+    // let payload_labels = payload_vars
+    //     .iter()
+    //     .enumerate()
+    //     .map(|(index, _)| format!("v{}", index))
+    //     .collect();
+
+    let tag_payload = payload_vars
+        .iter()
+        .enumerate()
+        .map(|(index, variable)| {
+            let expr = Expr::Var(env.new_symbol(format!("v{}", index)), *variable);
+            (*variable, Loc::at_zero(expr))
+        })
+        .collect();
+
+    let tag_var = env.subs.fresh_unnamed_flex_var();
+    let tag = Expr::Tag {
+        tag_union_var: tag_var,
+        ext_var: env.new_ext_var(ExtensionKind::TagUnion),
+        name: tag_name,
+        arguments: tag_payload,
+    };
+
+    let ok_branch_value = Expr::Tag {
+        tag_union_var: when_var,
+        ext_var: env.new_ext_var(ExtensionKind::TagUnion),
+        name: "Ok".into(),
+        arguments: vec![(tag_var, Loc::at_zero(tag))],
+    };
+
+    let record_pattern_destructs = payload_vars
+        .iter()
+        .enumerate()
+        .map(|(index, variable)| {
+            Loc::at_zero(RecordDestruct {
+                var: *variable,
+                label: "label".into(),
+                symbol: env.new_symbol("symbol"),
+                typ: DestructType::Required,
+            })
+        })
+        .collect();
+
+    let branches = vec![WhenBranch {
+        patterns: vec![WhenBranchPattern {
+            pattern: Loc::at_zero(Pattern::RecordDestructure {
+                whole_var: s_arg_variable,
+                ext_var: env.new_ext_var(ExtensionKind::Record),
+                destructs: record_pattern_destructs,
+            }),
+            degenerate: false,
+        }],
+        value: Loc::at_zero(ok_branch_value),
+        guard: None,
+        redundant: RedundantMark::known_non_redundant(),
+    }];
+
+    let when = Expr::When {
+        loc_cond: Box::new(Loc::at_zero(Expr::Var(s_arg_symbol, s_arg_variable))),
+        cond_var: s_arg_variable,
+        expr_var: when_var,
+        region: Region::zero(),
+        branches,
+        branches_cond_var: Variable::NAT,
+        exhaustive: ExhaustiveMark::known_exhaustive(),
+    };
+
+    (when, env.subs.fresh_unnamed_flex_var())
+}
